3c34436ee0819059f86856e9e09476093667c4f8
Titou <nestor.2005@gmail.com>
2021-05-24 19:03:25 -0300

    add create routes function

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 /* jshint esversion: 6 */
 
-module.exports = (application) => {
-  // core routes
-  require('./users.routes')(application)
-  require('./testing.routes')(application)
+module.exports = {
+  create(application) {
+    // core routes
+    require('./users.routes')(application)
+    require('./auth.routes')(application)
+    require('./testing.routes')(application)
+  }
 }
