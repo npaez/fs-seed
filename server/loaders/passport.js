aaccd013185ae824eb6905defc054659c799b49a
Titou <nestor.2005@gmail.com>
2021-05-24 19:03:55 -0300

    add passport local strategy | fix require user services from index file

--- server/loaders/passport.js
+++ server/loaders/passport.js
@@ -2,11 +2,26 @@
 const passport = require('passport');
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 const JwtStrategy = require('passport-jwt').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 // services
-const users = require('../services/user.services');
+const { users } = require('../services');
 
 module.exports = {
   create() {
+    // local strategy
+    passport.use(new LocalStrategy({
+      usernameField: 'email'
+    }, async (username, password, done) => {
+      const user = await users.getByField({ email: username }, false);
+
+      if (!user || !user.comparePassword(password)) {
+        return done(null, false, { error: 'wrong email or password'});
+      }
+
+      return done(null, user);
+    }));
+    
+    // jwt strategy
     passport.use(new JwtStrategy({
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET
