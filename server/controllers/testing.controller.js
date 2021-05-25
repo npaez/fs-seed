1ac6d121595296892699cb24f04a9d0629d1703d
Titou <nestor.2005@gmail.com>
2021-05-25 11:18:43 -0300

    add testing private route

--- server/controllers/testing.controller.js
+++ server/controllers/testing.controller.js
@@ -2,4 +2,10 @@
  * testing route
  * @route GET /
  */
-exports.home = (req, res) => res.send('fs-seed');
+exports.home = (req, res) => res.success('fs-seed', 200);
+
+/**
+ * testing private route
+ * @route GET /private
+ */
+ exports.homePrivate = (req, res) => res.success('fs-seed private', 200);
--- server/routes/testing.routes.js
+++ server/routes/testing.routes.js
@@ -5,9 +5,14 @@ const ctrl = require('../controllers/testing.controller');
 
 // middlewares
 const { rateLimiter } = require('../lib/middlewares/limiter.middlewares');
+const { jwtAuth } = require('../lib/middlewares/auth.middlewares');
 
 module.exports = (application) => {
   application
   .route('/')
   .get(rateLimiter, ctrl.home)
+
+  application
+  .route('/private')
+  .get(rateLimiter, jwtAuth, ctrl.homePrivate)
 }
