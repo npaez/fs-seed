f01659269547a821604f199078c962f1fc06351e
Titou <nestor.2005@gmail.com>
2023-03-26 21:43:39 -0300

    fix opts

--- server/loaders/mongodb.loader.js
+++ server/loaders/mongodb.loader.js
@@ -7,14 +7,14 @@ module.exports = {
     try {
       await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useFindAndModify: false,
         useUnifiedTopology: true,
+        useFindAndModify: false,
         useCreateIndex: true
       });
     } catch (ex) {
       console.log(`[-] mongodb exception ${ ex.message }`);
       return process.exit(1);
-    }
+    };
 
     return console.log(`[+] connected to mongodb - database: ${ process.env.MONGO_NAME }`);
   }
